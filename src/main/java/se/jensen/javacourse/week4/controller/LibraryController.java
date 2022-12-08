c6710601ce016528f5d4a4a33cb4b28d93c59354
Lukhasz <lukaskaarle@gmail.com>
2022-12-08 17:55:24 +0100

    Created base endpoints and used the methods in LibraryService

--- src/main/java/se/jensen/javacourse/week4/controller/LibraryController.java
+++ src/main/java/se/jensen/javacourse/week4/controller/LibraryController.java
@@ -1,4 +1,89 @@
 package se.jensen.javacourse.week4.controller;
 
-public class LibraryController
-{}
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import se.jensen.javacourse.week4.model.Artist;
+import se.jensen.javacourse.week4.model.Track;
+import se.jensen.javacourse.week4.service.LibraryService;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Map;
+
+@RestController
+@RequestMapping ("/api/v1")
+public class LibraryController {
+
+    @Autowired
+    LibraryService libraryService = new LibraryService();
+    private static final ArrayList<Artist> name = new ArrayList<>();
+
+
+    @GetMapping("/artists")
+    public Object getArtists(HttpServletRequest req) {
+
+        boolean hasNamesOnly = "true".equals(req.getParameter("namesOnly"));
+            if (hasNamesOnly) {
+                return libraryService.getArtistsNamesOnly();
+            }
+            return libraryService.getArtists();
+    }
+
+    @PostMapping("/artists")
+    public void createArtist(@RequestBody Artist body) {
+        Artist artist = new Artist(body.getName());
+        libraryService.createArtist(artist);
+    }
+
+    @PutMapping("/artists/{id}")
+    public void updateArtist(@PathVariable("id") int id, @RequestBody Artist body) {
+        libraryService.updateArtist(id, body);
+    }
+
+    @DeleteMapping("/artists/{id}")
+    public void deleteArtist(@PathVariable("id") int id) {
+        libraryService.deleteArtist(id);
+    }
+
+    @GetMapping("/tracks")
+    public void getTracks(HttpServletRequest req) {
+        libraryService.getTracks();
+    }
+
+    @PostMapping("/artists/{artistId}/tracks")
+    public void addTrack(@PathVariable("artistId") int id, @RequestBody Track body) {
+        libraryService.addTrack(id, body);
+    }
+
+    @PutMapping("/artists/{artistId}/tracks/{trackId}")
+    public void updateTrack(@PathVariable("artistId") int aid, @PathVariable("trackId") int tid, @RequestBody Track tbody) {
+        libraryService.updateTrack(aid, tid, tbody);
+    }
+
+    @DeleteMapping("/artists/{artistId}/tracks/{trackId}")
+    public void deleteTrack(@PathVariable("artistId") int aid, @PathVariable("trackId") int tid) {
+        libraryService.deleteTrack(aid, tid);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
